ec110d898db3bd868aa0bad14dc990cbdd64b0f0
Carlos Jimenez <cejtok@gmail.com>
2022-05-05 16:50:32 -0500

    Inicio Etapa B

---
+++ Assets/ItemB.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemB : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+
+    RectTransform rect;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+    
+    public void OnBeginDrag(PointerEventData eventdata)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventdata)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventdata)
+    {
+        rect.anchoredPosition += eventdata.delta;
+    }
+
+    public void OnPointerDown(PointerEventData eventdata)
+    {
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
