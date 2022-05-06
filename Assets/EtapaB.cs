58306b0cdb0cfc5ec572ae92795fd29b91a47e7a
Carlos Jimenez <cejtok@gmail.com>
2022-05-05 22:08:33 -0500

    terminada etapa B

--- Assets/EtapaB.cs
+++ Assets/EtapaB.cs
@@ -91,5 +91,9 @@ public class EtapaB : MonoBehaviour
         {
             readJSON();
         }
+
+        
+
+
     }
 }
--- Assets/ItemB.cs
+++ Assets/ItemB.cs
@@ -12,6 +12,13 @@ public class ItemB : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEnd
     public Text text_nombre;
     public Text text_nota;
 
+    public GameObject Alerta;
+    public Text text_alerta;
+
+
+    int col = 0;
+    Vector2 pos;
+
     void Awake()
     {
         rect = GetComponent<RectTransform>();
@@ -19,12 +26,102 @@ public class ItemB : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEnd
     
     public void OnBeginDrag(PointerEventData eventdata)
     {
+        pos = rect.anchoredPosition;
+    }
 
+    public bool getRevision()
+    {
+        if (col == 1)
+        {
+            return estudiante.nota >= 3.0f;
+        }
+        else
+        {
+            if (col == 2)
+            {
+                return estudiante.nota < 3.0f;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 
     public void OnEndDrag(PointerEventData eventdata)
     {
+        int newcol = Mathf.Clamp(Mathf.FloorToInt((rect.anchoredPosition.x+100) / 200.0f) + 1, 0, 2);       
 
+        if (newcol != col)
+        {
+            col = newcol;
+            tag = "col" + newcol;
+
+            rect.anchoredPosition = new Vector2((newcol - 1) * 200, rect.anchoredPosition.y);
+
+            GameObject[] list0 = GameObject.FindGameObjectsWithTag("col0");
+            GameObject[] list1 = GameObject.FindGameObjectsWithTag("col1");
+            GameObject[] list2 = GameObject.FindGameObjectsWithTag("col2");
+            int i = 0;
+            RectTransform rectg;
+            foreach (GameObject obj in list0) {
+                rectg = obj.GetComponent<RectTransform>();
+                rectg.anchoredPosition = new Vector2(rectg.anchoredPosition.x, -(5 * (i + 1) + 70 * i));
+                i++;
+            }
+            i = 0;
+            foreach (GameObject obj in list1)
+            {
+                rectg = obj.GetComponent<RectTransform>();
+                rectg.anchoredPosition = new Vector2(rectg.anchoredPosition.x, -(5 * (i + 1) + 70 * i));
+                i++;
+            }
+            i = 0;
+            foreach (GameObject obj in list2)
+            {
+                rectg = obj.GetComponent<RectTransform>();
+                rectg.anchoredPosition = new Vector2(rectg.anchoredPosition.x, -(5 * (i + 1) + 70 * i));
+                i++;
+            }
+
+            if((list0.Length == 0)&&((list1.Length>0)||(list2.Length>0)))
+            {
+                bool check = true;
+                foreach (GameObject obj in list1)
+                {
+                    if (obj.GetComponent<ItemB>().getRevision() == false)
+                    {
+                        check = false;
+                        break;
+                    }                        
+                }
+                foreach (GameObject obj in list2)
+                {
+                    if (obj.GetComponent<ItemB>().getRevision() == false)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                GameObject o = GameObject.Find("ScrollRectB");
+                Text textalerta = GameObject.Find("TextAlertaB").GetComponent<Text>();
+                o.SetActive(false);
+
+                if (check == true)
+                {
+                    textalerta.text = "PERFECTO:\nLos estudiantes estan ubicados correctamente";
+                }
+                else
+                {
+                    textalerta.text = "FALLASTE:\nLos estudiantes estan ubicados incorrectamente";
+                }
+            }
+
+        }
+        else
+        {
+            rect.anchoredPosition = pos;
+        }
     }
 
     public void OnDrag(PointerEventData eventdata)
@@ -45,6 +142,8 @@ public class ItemB : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEnd
         estudiante = e;
         text_nombre.text = e.nombre + " " + e.apellido;
         text_nota.text = "" + e.nota;
+        tag = "col0";
+        col = 0;
     }
 
 
